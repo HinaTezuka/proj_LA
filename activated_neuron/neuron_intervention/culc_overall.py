ed07e369393194b87d31e1439fc8e817e46cb593
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2024-12-08 09:41:39 +0900

    add/neuron_intervention_csv

--- activated_neuron/neuron_intervention/culc_overall.py
+++ activated_neuron/neuron_intervention/culc_overall.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 # CSVファイルを読み込んでデータフレームを作成
-path = "/home/s2410121/proj_LA/activated_neuron/neuron_intervention/csv_files/blimp/shared/n_20000/llama3_en_ja_shared_ONLY.csv"
+path = "/home/s2410121/proj_LA/activated_neuron/neuron_intervention/csv_files/blimp/shared/n_20000/llama3_en_ja_shared_non_translation.csv"
 df = pd.read_csv(path)
 
 # 各モデルごとに正答率の平均を計算します
